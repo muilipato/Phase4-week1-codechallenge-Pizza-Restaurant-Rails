e2bddabf55bc5fc10f89dd6cfe7eb613f5ce3c82
Patrick <muilipato@gmail.com>
2023-06-26 23:36:41 +0300

    added relationships to pizza.rb, restaurant.rb and restaurant_pizza.rb

--- app/models/restaurant_pizza.rb
+++ app/models/restaurant_pizza.rb
@@ -1,4 +1,13 @@
 class RestaurantPizza < ApplicationRecord
   belongs_to :restaurant
   belongs_to :pizza
+
+  validates :price, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 30 }
+
+=begin
+ Must have validation to the `RestaurantPizza` model:
+   must have a `price` between 1 and 30
+=end
+
+
 end
